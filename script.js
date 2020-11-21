248247084ff1736cf8e93bd014a593253e986185
Mark Williams <mwxxiii@gmail.com>
2020-11-21 09:11:20 +0000

    Added HTML, CSS and JS

---
+++ script.js
@@ -0,0 +1,49 @@
+
+var decisionButton = document.getElementById('decisionButton');
+var decisionOutput = document.getElementById('decisionOutput');
+
+function generateRandomNumber(num) { 
+    // Gets # from 0 -> num - 1
+    return Math.floor(Math.random() * num)
+  }
+  
+  const collectiveWisdom = {
+    signInfo: ['star', 'moon', 'sun', 'comet'],
+    fortuneOutput: ['terrible luck', 'bad luck', 'ok luck', 'good luck', 'great luck'],
+    advice: ['go out and eat', 'not read this', 'play more', 'trust no one']
+  }
+  
+  // Store the 'wisdom' in an array
+  let personalWisdom = []
+  
+  // Iterate over the object
+  for(let prop in collectiveWisdom) {
+    let optionIdx = generateRandomNumber(collectiveWisdom[prop].length)
+  
+    // use the object's properties to customize the message being added to personalWisdom  
+    switch(prop) {
+      case 'signInfo':
+        personalWisdom.push(`Your sign right now is a "${collectiveWisdom[prop][optionIdx]}".`)
+        break
+      case 'fortuneOutput':
+        personalWisdom.push(`You are having: "${collectiveWisdom[prop][optionIdx]}".`)
+        break
+      case 'advice':
+        personalWisdom.push(`You should: "${collectiveWisdom[prop][optionIdx]}".`)
+        break
+      default:
+        personalWisdom.push('There is not enough info.')
+    }
+  }
+  
+  function formatWisdom(wisdom) {
+    const formatted = personalWisdom.join('\n')
+    return formatted;
+    //console.log(formatted);
+  }
+    //formatWisdom(personalWisdom)
+
+decisionButton.onclick = function () {
+  document.getElementById("decisionOutput").innerHTML=formatWisdom(personalWisdom);
+  return false;
+}
